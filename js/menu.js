9118c1a017c5c5347593e140e6a93411ffcb3f99
RyanVictorDev <vryan8294@gmail.com>
2024-02-24 11:14:23 -0300

    Configurando menu responsivo

--- js/menu.js
+++ js/menu.js
@@ -2,3 +2,12 @@ window.addEventListener("scroll", function(){
     let header = document.querySelector('#header');
     header.classList.toggle('rolagem', window.scrollY > 0)
 })
+
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+    } else {
+        menuMobile.classList.add('open');
+    };
+};
